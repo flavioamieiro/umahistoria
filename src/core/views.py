534d4101b6b4b75c496ef88f4afbb03e79e1a447
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-03-20 02:03:22 -0300

    Corrige espaçamento entre as classes

--- src/core/views.py
+++ src/core/views.py
@@ -7,20 +7,19 @@ from models import Chapter
 from forms import ChapterForm
 
 class ChapterListView(ListView):
-
     template_name = "core/index.html"
     context_object_name = "chapters"
     queryset = Chapter.objects.filter(day=date.today())
 
 
 class NewChapterView(CreateView):
-
     template_name = "core/novo_capitulo.html"
     form_class = ChapterForm
 
     @property
     def success_url(self):
         return reverse("core:index")
+
 
 class SobreView(TemplateView):
     template_name = "core/sobre.html"
